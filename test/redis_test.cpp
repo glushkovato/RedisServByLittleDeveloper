7de44e42ba9c6e035ed199bccdf30d576a6eaa6f
glushkovato <glushkovato@gmail.com>
2015-11-04 14:00:29 +0300

    add tests for type REDIS_ARRAY

--- test/redis_test.cpp
+++ test/redis_test.cpp
@@ -6,7 +6,6 @@ TEST(RedisValue, Construct) {
     RedisValue integer = 10;
     RedisValue string = "abcd";
     RedisValue error = RedisError("Permission denied");
-
     RedisValue array = std::vector<RedisValue>{integer, string, error};
 }
 
@@ -59,3 +58,37 @@ TEST(ReadRedisValue, string) {
 
     EXPECT_STREQ("lol", boost::get<std::string>(val).c_str());
 }
+
+TEST(WriteRedisValue, Array) {
+    RedisValue integer = 10;
+    RedisValue string = "lol";
+    RedisValue error = RedisError("Permission denied");
+    RedisValue array = std::vector<RedisValue>{integer, string, error};
+
+    StringWriter writer(1024);
+    WriteRedisValue(&writer, array);
+    writer.flush();
+
+    EXPECT_STREQ("*3\r\n:10\r\n+lol\r\n-Permission denied\r\n", writer.result.c_str());
+}
+
+TEST(ReadRedisValue, Array) {
+    RedisValue val;
+
+    StringReader reader;
+
+    reader.input = "*3\r\n:10\r\n+lol\r\n-Permission denied\r\n";
+    ReadRedisValue(&reader, &val);
+
+    EXPECT_EQ(10, boost::get<int64_t>(boost::get<std::vector<RedisValue>>(val)[0]));
+    EXPECT_STREQ("lol", boost::get<std::string>(boost::get<std::vector<RedisValue>>(val)[1]).c_str());
+    EXPECT_STREQ("Permission denied", boost::get<RedisError>(boost::get<std::vector<RedisValue>>(val)[2]).msg.c_str());
+}
+
+
+
+
+
+
+
+
