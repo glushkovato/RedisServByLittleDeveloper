227a3c29b15c446359d571d8480b8cbc6f225f3e
glushkovato <glushkovato@gmail.com>
2015-12-14 14:24:15 +0300

    added ServerListener test (accept_conn)

--- test/redis_test.cpp
+++ test/redis_test.cpp
@@ -1,6 +1,8 @@
 #include "protocol/redis.h"
 #include <boost/algorithm/string/join.hpp>
 #include <gtest/gtest.h>
+#include <server/listener.h>
+#include <thread>
 
 TEST(RedisValue, Construct) {
     RedisValue integer = 10;
@@ -38,6 +40,25 @@ TEST(ReadRedisValue, Int) {
     reader.input = ":-5\r\n";
     ReadRedisValue(&reader, &val);
     EXPECT_EQ(-5, boost::get<int64_t>(val));
+
+    try {
+        reader.input = ":-1234567898765432123456789\r\n";
+        ReadRedisValue(&reader, &val);
+        //EXPECT_THROW(ReadRedisValue(&reader, &val), std::invalid_argument);
+    } catch(std::invalid_argument &e) {
+        EXPECT_STREQ(e.what(), "bad value of integer");
+    } catch(...) {
+        FAIL() << "Unknown exception";
+    }
+
+    try {
+        reader.input = ":9223372036854775807\r\n";
+        ReadRedisValue(&reader, &val);
+    } catch(std::invalid_argument &e) {
+        EXPECT_STREQ(e.what(), "bad value of integer");
+    } catch(...) {
+        //FAIL() << "Unknown exception";
+    }
 }
 
 TEST(WriteRedisValue, string) {
@@ -48,6 +69,8 @@ TEST(WriteRedisValue, string) {
     writer.flush();
 
     EXPECT_STREQ("+lol\r\n", writer.result.c_str());
+
+
 }
 
 TEST(ReadRedisValue, string) {
@@ -58,6 +81,41 @@ TEST(ReadRedisValue, string) {
     ReadRedisValue(&reader, &val);
 
     EXPECT_STREQ("lol", boost::get<std::string>(val).c_str());
+}
+
+TEST(WriteRedisValue, BulkString){
+    RedisValue val1 = std::vector<char> {'l', 'o', 'l'};
+    RedisValue val2 = std::vector<char> {};
+
+    StringWriter writer(1024);
+    WriteRedisValue(&writer, val1);
+    writer.flush();
+
+    EXPECT_STREQ("$3\r\nlol\r\n", writer.result.c_str());
+
+    writer.result.erase(writer.result.begin(), writer.result.end());
+
+//    StringWriter writer(1024);
+    WriteRedisValue(&writer, val2);
+    writer.flush();
+
+    EXPECT_STREQ("$0\r\n\r\n", writer.result.c_str());
+
+}
+
+TEST(ReadRedisValue, BulkString) {
+    RedisValue val;
+
+    StringReader reader;
+    reader.input = "$3\r\nlol\r\n";
+    ReadRedisValue(&reader, &val);
+
+    EXPECT_TRUE((std::vector<char>{'l', 'o', 'l'} == boost::get<std::vector<char>>(val)));
+
+//    StringReader reader;
+    reader.input = "$0\r\n\r\n";
+    ReadRedisValue(&reader, &val);
+    EXPECT_TRUE((std::vector<char>{} == boost::get<std::vector<char>>(val)));
 }
 
 TEST(WriteRedisValue, redisError) {
@@ -131,10 +189,34 @@ TEST(ReadRedisValue, Array) {
     EXPECT_EQ(0, boost::get<std::vector<RedisValue>>(val).size());
 }
 
+void thread_func(int port) {  // всё, что необходимо для создания клиентского сокета
+    int socket_desc;
+    struct sockaddr_in server;
 
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");  //
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
 
+    //Подключение к Listener
+    (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)));
+}
 
+TEST(ServerListener, accept_conn) {
+    int port = 9999;
+    Listener lsn(port);
+
+    std::thread thr(thread_func, port);
+    if (lsn.accept_conn() == nullptr) {
+        FAIL();
+    } else {
+        SUCCEED();
+    }
+    thr.join();
+
+}
 
 
 
